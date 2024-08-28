43123f24cf678f1157d3a831f5862137f6af68eb
heeom <swimxdev@gmail.com>
2024-08-28 20:58:24 +0900

    feat : PessimisticLock 구현하기

---
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,23 @@
+package com.example.stock.service;
+
+import com.example.stock.entity.Stock;
+import com.example.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+        stockRepository.save(stock);
+    }
+}
